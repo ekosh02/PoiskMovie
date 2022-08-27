b9afd30f96c4372388b97a55647041ea7628596c
ekosh02 <turapbaeveldos@gmail.com>
2022-08-27 22:48:13 +0600

    text style

---
+++ lib/style/text_styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle title1Bold = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  static const TextStyle title1Semibold = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static const TextStyle title2Semibold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static const TextStyle title2Regular = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  //---------------------------------------------------------
+
+  static const TextStyle headline1Regular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle headline1RegularGray = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff333741),
+  );
+
+  //---------------------------------------------------------
+
+  static const TextStyle text1Regular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle text1Medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+}
--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_app/style/text_styles.dart';
 
 class AuthWidget extends StatefulWidget {
   const AuthWidget({Key? key}) : super(key: key);
@@ -40,26 +41,16 @@ class _HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const headerTextStyle = TextStyle(
-      fontSize: 26,
-      fontWeight: FontWeight.w500,
-      color: Colors.black,
-    );
-    const descriptionTextStyle = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w400,
-      color: Colors.black,
-    );
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 16),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(headerText, style: headerTextStyle),
+          Text(headerText, style: TextStyles.title2Semibold),
           const SizedBox(height: 10),
-          Text(descriptionText1, style: descriptionTextStyle),
+          Text(descriptionText1, style: TextStyles.text1Regular),
           const SizedBox(height: 10),
-          Text(descriptionText2, style: descriptionTextStyle),
+          Text(descriptionText2, style: TextStyles.text1Regular),
         ],
       ),
     );
@@ -75,26 +66,21 @@ class _FormWidget extends StatefulWidget {
 class _FormWidgetState extends State<_FormWidget> {
   @override
   Widget build(BuildContext context) {
-    const formTextStyle = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w400,
-      color: Color(0xff333741),
-    );
     const textFieldDecaration = InputDecoration(
       border: OutlineInputBorder(),
       contentPadding: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
       isCollapsed: true,
     );
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 16),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
-          Text('Username', style: formTextStyle),
+          Text('Username', style: TextStyles.headline1RegularGray),
           SizedBox(height: 8),
           TextField(decoration: textFieldDecaration),
           SizedBox(height: 16),
-          Text('Password', style: formTextStyle),
+          Text('Password', style: TextStyles.headline1RegularGray),
           SizedBox(height: 8),
           TextField(decoration: textFieldDecaration),
         ],
