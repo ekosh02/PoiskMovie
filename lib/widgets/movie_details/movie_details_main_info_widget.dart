d3f0585a7cf27625a35057be36c4b9be013b382c
ekosh02 <turapbaeveldos@gmail.com>
2022-08-31 00:44:17 +0600

    added movie name widget

--- lib/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/widgets/movie_details/movie_details_main_info_widget.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tmdb_app/resources/app_images.dart';
+import 'package:tmdb_app/style/color_set.dart';
+import 'package:tmdb_app/style/text_styles.dart';
 
 class MovieDetailsMainInfoWidget extends StatelessWidget {
   const MovieDetailsMainInfoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        _TopPosterWidget(),
-      ],
+    return ColoredBox(
+      color: ColorSet.dartTheme,
+      child: ListView(
+        children: const [
+          _TopPosterWidget(),
+          _MovieNameWidget(),
+        ],
+      ),
     );
   }
 }
@@ -29,6 +35,29 @@ class _TopPosterWidget extends StatelessWidget {
           child: Image(image: AssetImage(AppImages.mars)),
         ),
       ],
+    );
+  }
+}
+
+class _MovieNameWidget extends StatelessWidget {
+  const _MovieNameWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Center(
+        child: RichText(
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          text: const TextSpan(
+            children: [
+              TextSpan(text: 'Mars', style: TextStyles.title1SemiboldWhite),
+              TextSpan(text: '(2022)', style: TextStyles.headline1RegularWhite),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
