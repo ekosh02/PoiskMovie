7f2a5c44f4a053fca2c191b39daded9cbc16eb1e
ekosh02 <turapbaeveldos@gmail.com>
2022-08-30 22:21:19 +0600

    added image movie in details main info

--- lib/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/widgets/movie_details/movie_details_main_info_widget.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_app/resources/app_images.dart';
 
-class MovieDetailsMainInfoWidget extends StatelessWidget {
+class MovieDetailsMainInfoWidget extends StatefulWidget {
   const MovieDetailsMainInfoWidget({Key? key}) : super(key: key);
 
   @override
+  State<MovieDetailsMainInfoWidget> createState() =>
+      _MovieDetailsMainInfoWidgetState();
+}
+
+class _MovieDetailsMainInfoWidgetState
+    extends State<MovieDetailsMainInfoWidget> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: const [
+        Image(image: AssetImage(AppImages.andromeda)),
+        Positioned(
+          left: 16,
+          top: 20,
+          bottom: 20,
+          child: Image(image: AssetImage(AppImages.mars)),
+        ),
+      ],
+    );
   }
 }
